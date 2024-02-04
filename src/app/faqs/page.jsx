8011e9e85b99eecb80309964c6191a57f85c6457
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2024-02-04 23:06:24 +0530

    dfond

--- src/app/faqs/page.jsx
+++ src/app/faqs/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import Cta from "../components/Cta";
+import Cta from "../../components/Cta";
 import { Disclosure } from "@headlessui/react";
 import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
