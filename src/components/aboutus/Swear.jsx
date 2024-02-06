1da16fc55ed4d0cde05011dc373193ccbefc80fb
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2024-02-06 13:23:04 +0530

    done

--- src/components/aboutus/Swear.jsx
+++ src/components/aboutus/Swear.jsx
@@ -40,9 +40,9 @@ export default function Swear() {
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
             {features.map((feature) => (
-              <div key={feature.name} className="relative pl-16">
+              <div key={feature.name} className="relative pl-16 hover:bg-gray-200 hover:scale-95 p-4 rounded-lg">
                 <dt className="text-base font-semibold leading-7 text-gray-900">
-                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                  <div className="absolute left-2 top-2 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                     <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
                   </div>
                   {feature.name}
